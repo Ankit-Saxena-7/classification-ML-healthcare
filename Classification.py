13816813e1947853fec4a1d9c7d6fde448d17c80
Ankit Saxena <54916905+Ankit-Saxena-7@users.noreply.github.com>
2020-09-01 15:32:49 -0400

    Add files via upload

--- Classification.py
+++ Classification.py
@@ -1,11 +1,12 @@
 # Import libraries
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import mean_squared_error as MSE, accuracy_score
-from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import mean_squared_error as MSE, accuracy_score, roc_auc_score
+from sklearn.ensemble import VotingClassifier, BaggingClassifier, RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+import matplotlib.pyplot as plt
 
 SEED = 1
 
@@ -56,7 +57,7 @@ print('Baseline MSE: {:.2f}'.format(BaselineRMSE))
 
 print('Model has high bias')
 
-# METHOD II: Ensemble models
+# METHOD II: Voting classifier
 
 vLogisticRegression = LogisticRegression(random_state=SEED, max_iter=3000)
 vKNeighbors = KNeighborsClassifier(n_neighbors=27)
@@ -84,3 +85,97 @@ vYPred = vVotingClassifier.predict(vXTest)
 vAccuracy = accuracy_score(vYPred, vYTest)
 print('{:s} : {:.3f}'.format('Voting Classifier', vAccuracy))
 
+# METHOD III: Bagging classifier (sampling with repetition)
+vBaggingClassifier = BaggingClassifier(base_estimator=vDecisionTree, n_estimators=50, random_state=SEED, oob_score=True)
+vBaggingClassifier.fit(vXTrain, vYTrain)
+vYPred = vBaggingClassifier.predict(vXTest)
+vAccuracy = accuracy_score(vYPred, vYTest)
+vOOBAccuracy = vBaggingClassifier.oob_score_
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(vAccuracy, vOOBAccuracy))
+
+# METHOD IV: Random forest
+vRandomForestClassifier = RandomForestClassifier(n_estimators=25, random_state=SEED)
+vRandomForestClassifier.fit(vXTrain, vYTrain)
+vYPred = vRandomForestClassifier.predict(vXTest)
+vRMSE = MSE(vYTest, vYPred)**(1/2)
+print('Test set RMSE of Random Forest: {:.2f}'.format(vRMSE))
+
+# Create a pd.Series of features importances
+vImportances = pd.Series(data=vRandomForestClassifier.feature_importances_,
+                        index= vXTrain.columns)
+
+# Sort importances
+vImportancesSorted = vImportances.sort_values()
+
+# Draw a horizontal barplot of importances_sorted
+vImportancesSorted.plot(kind='barh', color='lightgreen')
+plt.title('Features Importances')
+plt.show()
+
+# METHOD V: Boosting
+# Ada Boosting
+vAdaBoostClassifier = AdaBoostClassifier(base_estimator=vDecisionTree, n_estimators=180, random_state=SEED)
+
+# Fit ada to the training set
+vAdaBoostClassifier.fit(vXTrain, vYTrain)
+
+# Compute the probabilities of obtaining the positive class
+vYPredProba = vAdaBoostClassifier.predict_proba(vXTest)[:, 1]
+vAdaROCAUC = roc_auc_score(vYTest, vYPredProba)
+print('ROC AUC score: {:.2f}'.format(vAdaROCAUC))
+
+# Gradient Boosting
+vGradientBoostingClassifier = GradientBoostingClassifier(max_depth=4, n_estimators=180, random_state=SEED)
+vGradientBoostingClassifier.fit(vXTrain, vYTrain)
+vYPred = vGradientBoostingClassifier.predict(vXTest)
+vRMSE = MSE(vYTest, vYPred)**(1/2)
+print('Test set RMSE of Gradient Boosting Classifier: {:.2f}'.format(vRMSE))
+
+# Stochastic Gradient Boosting
+vStochasticGradientBoostingClassifier = GradientBoostingClassifier(max_depth=4, n_estimators=180, random_state=SEED, subsample=0.9, max_features=0.75)
+vStochasticGradientBoostingClassifier.fit(vXTrain, vYTrain)
+vYPred = vStochasticGradientBoostingClassifier.predict(vXTest)
+vRMSE = MSE(vYTest, vYPred)**(1/2)
+print('Test set RMSE of Stochastic Gradient Boosting Classifier: {:.2f}'.format(vRMSE))
+
+# METHOD VI: Hyperparameter tuning of decision tree
+print(vDecisionTree.get_params())
+
+vParamsDecisionTree = {
+    'max_depth': [2, 3, 4],
+    'min_samples_leaf': [0.12, 0.14, 0.16, 0.18]
+}
+
+VGridDecisionTree = GridSearchCV(   estimator=vDecisionTree,
+                                    param_grid=vParamsDecisionTree,
+                                    scoring='roc_auc',
+                                    cv=5,
+                                    n_jobs=-1)
+
+VGridDecisionTree.fit(vXTrain, vYTrain)
+vBestModel = VGridDecisionTree.best_estimator_
+vYPredProba = vBestModel.predict_proba(vXTest)[:, 1]
+vAdaROCAUC = roc_auc_score(vYTest, vYPredProba)
+print('ROC AUC score: {:.2f}'.format(vAdaROCAUC))
+
+# METHOD VII: Hyperparameter tuning of random forest
+print(vRandomForestClassifier.get_params())
+
+vParamsRandomForest = {
+    'n_estimators': [100, 350, 500],
+    'max_features': ['log2', 'auto', 'sqrt'],
+    'min_samples_leaf': [2,10,30]
+}
+
+VGridRandomForest = GridSearchCV(estimator=vRandomForestClassifier,
+                       param_grid=vParamsRandomForest,
+                       scoring='neg_mean_squared_error',
+                       cv=3,
+                       verbose=1,
+                       n_jobs=-1)
+
+VGridRandomForest.fit(vXTrain, vYTrain)
+vBestModel = VGridRandomForest.best_estimator_
+vYPred = vBestModel.predict(vXTest)
+vRMSE = MSE(vYTest, vYPred)**(1/2)
+print('Test set RMSE of Grid CV Random Forest: {:.2f}'.format(vRMSE))
